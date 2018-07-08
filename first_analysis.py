67b2c44033488299f9fd9d6e36669e886d7304d9
MorBoos <moritz.boos@gmail.com>
2018-07-08 20:00:54 +0200

    first commit

---
+++ first_analysis.py
@@ -0,0 +1,23 @@
+from __future__ import division
+from json import loads
+import pandas as pd
+from itertools import chain
+import numpy as np
+import spacy
+from sklearn.
+import matplotlib.pyplot as plt
+import preprocessing as pre
+import seaborn as sns
+sns.set_style('whitegrid')
+#TODO: convert into scipy sparse matrix format with embedder
+#TODO: convert oz into mg
+#TODO: sort by exoticness
+
+drinks = pre.load_data()
+
+ingredients = pd.Series(drinks[['strIngredient{}'.format(i) for i in range(1,16)]].values.flatten())
+ingredient_counts = ingredients.value_counts()
+ingredient_counts.iloc[ingredient_counts.values>1].iloc[::-1].plot.barh(figsize=(8,25))
+plt.savefig('common_ingredients.svg')
+
+
---
+++ preprocessing.py
@@ -0,0 +1,55 @@
+from __future__ import division
+from json import loads
+import pandas as pd
+from itertools import chain
+import numpy as np
+import spacy
+import re
+
+#TODO: simple word preprocessing
+def preprocess_ingredient(text):
+    import unicodedata as ud
+    if pd.isna(text):
+        return text
+    text = ud.normalize('NFD', text.encode('utf-8').decode('utf-8'))
+    text = text.lower()
+    text = re.sub(r'[\n\r]', r' ', text)
+    if text.isspace():
+        text = np.nan
+    return text
+
+def preprocess_measure(text):
+    import unicodedata as ud
+    if pd.isna(text):
+        return text
+    text = ud.normalize('NFD', text.encode('utf-8').decode('utf-8'))
+    text = text.lower()
+    text = re.sub(r'[\n\r]', r' ', text)
+    if text.isspace():
+        text = np.nan
+    return text
+
+def load_data(preprocess=True):
+    data = pd.read_csv('all_drinks.csv', encoding='utf-8')
+#    data.fillna('NAN', inplace=True)
+    if preprocess:
+        data.iloc[:,data.columns.str.contains('Ingredient')] = data.iloc[:,data.columns.str.contains('Ingredient')].applymap(preprocess_ingredient)
+        data.iloc[:,data.columns.str.contains('Measure')] = data.iloc[:,data.columns.str.contains('Measure')].applymap(preprocess_measure)
+    return data
+
+def make_tokenizer_dict(data):
+    ingredients = pd.Series(data[['strIngredient{}'.format(i) for i in range(1,16)]].values.flatten())
+    ingredient_counts = ingredients.value_counts()
+    token_to_word = {0 : np.nan}
+    word_to_token = {np.nan : 0}
+    for i, (ingredient, n) in enumerate(ingredient_counts.items()):
+        token_to_word[i] = ingredient
+        word_to_token[ingredient] = i
+    return token_to_word, word_to_token
+
+def tokenize_data(data):
+    token2word, word2token = make_tokenizer_dict(data)
+    data.iloc[:,data.columns.str.contains('Ingredient')] = data.iloc[:,data.columns.str.contains('Ingredient')].applymap(lambda x : word2token[x])
+    return data, token2word, word2token
+
+list_of_alcohol = ['vodka', 'rum', 'whiskey', 'tequila', 'sambuca', 'curacao']
