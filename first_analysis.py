6f760c4635f183226639a8edd5147e3156c1cfda
mjboos <moritz.boos@uni-oldenburg.de>
2018-07-09 19:09:53 +0200

    optimal exclusion of cocktails based on ingredients

--- first_analysis.py
+++ first_analysis.py
@@ -3,11 +3,28 @@ from json import loads
 import pandas as pd
 from itertools import chain
 import numpy as np
-import spacy
 import matplotlib.pyplot as plt
 import preprocessing as pre
+from sklearn.preprocessing import MultiLabelBinarizer
 import seaborn as sns
+
 sns.set_style('whitegrid')
+
+def stepwise_exclusion(included_ingredients, ingredients_vector):
+    '''Finds the ingredient to exclude that leaves as much drink options as possible
+    INPUT
+    included_ingredients    -       binary vector of length n, indicating which ingredients are included
+    ingredients_vector      -       binary array of shape (n_recipes,n), indicating which ingredients are necessary for a given recipe
+    OUTPUT
+    binary vector of length n with one less ingredient'''
+    cocktails_to_consider = np.logical_not(ingredients_vector[:,included_ingredients==0].any(axis=1))
+#    masked_recipes = np.ma.masked_array(ingredients_vector, np.tile(np.logical_not(included_ingredients)[None],(ingredients_vector.shape[0],1)))
+    masked_recipes = np.ma.masked_array(ingredients_vector, np.logical_or(np.logical_not(cocktails_to_consider)[:,None], np.tile(np.logical_not(included_ingredients)[None], (ingredients_vector.shape[0],1))))
+    ingredient_to_exclude = masked_recipes.sum(axis=0).argmin()
+    new_included_ingredients = included_ingredients.copy()
+    new_included_ingredients[ingredient_to_exclude] = 0
+    return new_included_ingredients
+
 #TODO: convert into scipy sparse matrix format with embedder
 #TODO: convert oz into mg
 #TODO: sort by exoticness
@@ -19,4 +36,29 @@ ingredient_counts = ingredients.value_counts()
 #ingredient_counts.iloc[ingredient_counts.values>1].iloc[::-1].plot.barh(figsize=(8,25))
 #plt.savefig('common_ingredients.svg')
 
-tokenized_drinks, token2word, word2token = pre.tokenize_data(drinks)
+ingredients_vector, mlb = pre.tokenize_data(drinks)
+
+included_ingredients = np.ones(ingredients_vector.shape[1])
+
+reduced_ingredients = [included_ingredients]
+cocktails_left = [ingredients_vector.shape[0]]
+excluded_cocktail = ['none']
+
+for i in range(ingredients_vector.shape[1]):
+    reduced_ingredients.append(stepwise_exclusion(reduced_ingredients[-1], ingredients_vector))
+    cocktails_left.append(np.logical_not(ingredients_vector[:, reduced_ingredients[-1]==0].any(axis=1)).sum())
+    excluded_cocktail.append(mlb.classes_[np.where(reduced_ingredients[-2]-reduced_ingredients[-1])])
+
+sns.set_style('white')
+fig, ax = plt.subplots(1,1,figsize=(10,5))
+ax.plot(np.arange(309),cocktails_left)
+#plt.xticks(np.arange(309), excluded_cocktail, rotation=90)
+fig.savefig('cocktails_per_ingredients_excluded.png')
+plt.xlabel('Ingredients excluded')
+plt.ylabel('Cocktail recipes')
+plt.close()
+
+ingredient_list_to_use = reduced_ingredients[-50]
+print('Uns bleiben {} Rezepte mit diesen Zutaten:'.format(cocktails_left[-50]))
+print(mlb.classes_[np.where(ingredient_list_to_use)])
+
--- preprocessing.py
+++ preprocessing.py
@@ -3,8 +3,8 @@ from json import loads
 import pandas as pd
 from itertools import chain
 import numpy as np
-import spacy
 import re
+from sklearn.preprocessing import MultiLabelBinarizer
 
 #TODO: simple word preprocessing
 def preprocess_ingredient(text):
@@ -37,19 +37,25 @@ def load_data(preprocess=True):
         data.iloc[:,data.columns.str.contains('Measure')] = data.iloc[:,data.columns.str.contains('Measure')].applymap(preprocess_measure)
     return data
 
+def tokenize_data(data):
+    mlb = MultiLabelBinarizer()
+    ingredients_series = data[['strIngredient{}'.format(i) for i in range(1,16)]].apply(lambda x : x.dropna().unique().tolist(), axis=1)
+    transformed_ingredients = mlb.fit_transform(ingredients_series)
+    return transformed_ingredients, mlb
+
 def make_tokenizer_dict(data):
     ingredients = pd.Series(data[['strIngredient{}'.format(i) for i in range(1,16)]].values.flatten())
     ingredient_counts = ingredients.value_counts()
-    token_to_word = {0 : np.NaN}
-    word_to_token = {np.NaN : 0}
+    token_to_word = {0:np.NaN}
+    word_to_token = {0:0}
     for i, (ingredient, n) in enumerate(ingredient_counts.items()):
-        token_to_word[i] = ingredient
-        word_to_token[ingredient] = i
+        token_to_word[i+1] = ingredient
+        word_to_token[ingredient] = i+1
     return token_to_word, word_to_token
 
-def tokenize_data(data):
+def tokenize_by_hand_data(data):
     token2word, word2token = make_tokenizer_dict(data)
-    data[['strIngredient{}'.format(i) for i in range(1,16)]] = data[['strIngredient{}'.format(i) for i in range(1,16)]].applymap(lambda x : word2token[x])
+    data[['strIngredient{}'.format(i) for i in range(1,16)]] = data[['strIngredient{}'.format(i) for i in range(1,16)]].fillna(0).applymap(lambda x : word2token[x])
     return data, token2word, word2token
 
 list_of_alcohol = ['vodka', 'rum', 'whiskey', 'tequila', 'sambuca', 'curacao']
